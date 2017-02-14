fb55434a6435154aa9fa6a45c2df16de9d49c05a
AdamNagao <nagaoadam223@gmail.com>
2017-02-14 14:28:40 -0800

    Created Main Class, PID Class, setup some methods

--- project/src/project/Main.java
+++ project/src/project/Main.java
@@ -1,5 +1,39 @@
 package project;
 
+import lejos.hardware.Button;
+import lejos.hardware.lcd.TextLCD;
+import lejos.hardware.motor.Motor;
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3GyroSensor;
+import lejos.hardware.sensor.HiTechnicGyro;
+
 public class Main {
 
+	static float kp = 1;
+	static float ki = 1;
+	static float kd = 1;
+	static float output;
+	static EV3GyroSensor Gyro = new EV3GyroSensor(SensorPort.S1);
+
+	
+	public static void main(String[] args){
+		TextLCD.drawString("Position the Robot for Calibration, then hit enter",0,0);
+		Button.ENTER.waitForPressAndRelease();
+		Gyro.reset();  //Zero the gyro
+		Gyro.getAngleMode(); //Tell the gyro we are in angular velocity mode
+		PID.SetTunings(kp, ki,kd); //Set out PIDs
+		
+		
+		Button.ENTER.waitForPressAndRelease();
+		while(true){
+			//Calculate the output of the PID Alg
+			PID.Compute();
+			
+			//Set Motors to calculated output
+			Motor.A.setSpeed(output);
+			Motor.B.setSpeed(output);
+		}
+	}
 }
+
+
--- project/src/project/PID.java
+++ project/src/project/PID.java
@@ -2,4 +2,31 @@ package project;
 
 public class PID {
 
+	
+	/*working variables**/
+	static float lastTime;
+	static float Input, Output, Setpoint;
+	static float errSum, lastErr;
+	static float kp, ki, kd;
+	
+	public static void Compute(){
+		/*How long since we last calculated*/
+		float now = millis();
+		float timeChange =(now - lastTime);
+		/*Compute all the working error variables*/
+		float error = Setpoint - Input;
+		errSum += (error * timeChange);
+		float dErr = (error - lastErr) / timeChange;
+		/*Compute PID Output*/
+		Output = kp * error + ki * errSum + kd * dErr;
+		/*Remember some variables for next time*/
+		lastErr = error;
+		lastTime = now;
+	}
+	
+	public static void SetTunings(float Kp, float Ki, float Kd){
+		kp = Kp;
+		ki = Ki;
+		kd = Kd;
+	}
 }
